e3d521cc4ddf235bb9d1561c3dc5d45558541866
Sick the Cat <129430216+sickthecat@users.noreply.github.com>
2023-05-08 20:53:08 -0400

    Update pgp-afterburn.py
    newline and chars fix.

--- pgp-afterburn.py
+++ pgp-afterburn.py
@@ -1,42 +1,38 @@
 import os
-import string
 import gnupg
+import getpass
 
-def generate_password(length=16):
-    """Generate a random password with the specified length."""
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join([alphabet[ord(os.urandom(1)) % len(alphabet)] for i in range(length)])
-    return password
-
-def generate_keypair(name, email, passphrase, private_key_file='private.key', public_key_file='public.key'):
-    """Generate an RSA PGP keypair with the specified name, email, and passphrase and save it to files."""
+def generate_keypair(name, email, passphrase, key_type='RSA', key_length=4096, subkey_type='RSA', subkey_length=4096):
+    """Generate a PGP keypair with the specified parameters and save it to disk."""
     gpg = gnupg.GPG()
     input_data = gpg.gen_key_input(
-        key_type='RSA',
-        key_length=4096,
+        key_type=key_type,
+        key_length=key_length,
+        subkey_type=subkey_type,
+        subkey_length=subkey_length,
         name_real=name,
         name_email=email,
-        passphrase=passphrase
+        passphrase=passphrase,
     )
     key = gpg.gen_key(input_data)
     private_key = gpg.export_keys(key.fingerprint, True, passphrase=passphrase)
     public_key = gpg.export_keys(key.fingerprint)
-    with open(private_key_file, 'w') as f:
+    with open('private.key', 'w') as f:
         f.write(private_key)
-    with open(public_key_file, 'w') as f:
+    with open('public.key', 'w') as f:
         f.write(public_key)
-    return key
-
-if __name__ == '__main__':
-    password = generate_password()
-    print('Generated Password:', password)
-
-    name = input('Enter your name: ')
-    email = input('Enter your email address: ')
-    passphrase = input('Enter a passphrase for your PGP key: ')
-
-    key = generate_keypair(name, email, passphrase)
-    fingerprint = key.fingerprint
-    print('Generated PGP key with fingerprint:', fingerprint)
+    print('Generated PGP key with fingerprint:', key.fingerprint)
     print('Private key saved to private.key')
     print('Public key saved to public.key')
+
+def get_password():
+    """Prompt the user to enter a password."""
+    password = getpass.getpass('Enter your password: ')
+    return password.strip()
+
+if __name__ == '__main__':
+    name = input('Enter your name: ')
+    email = input('Enter your email address: ')
+    password = get_password()
+
+    generate_keypair(name, email, password)
